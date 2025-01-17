9e41d6899ad6b3803c8a34f9f8f3be91964dad61
AdamJoestar <adamschoolauto@gmail.com>
2025-01-17 17:38:46 +0700

    part 6

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,61 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home', ['title' => 'HomePage' ]);
+    return view('home', ['title' => 'HomePage']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['name' => 'RudyRacing'], ['title' => 'AboutPage' ]);
+    return view('about', ['name' => 'RudyRacing'], ['title' => 'AboutPage']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'BlogPage' ]);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'BlogPage', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'RudyRacing',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorum similique, architecto distinctio velit facilis itaque dolorem non. Architecto quo dicta quaerat dolorum error, nulla exercitationem suscipit voluptatem consequuntur unde.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'JunaidiRacing',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorum similique, architecto distinctio velit facilis itaque dolorem non. Architecto quo dicta quaerat dolorum error, nulla exercitationem suscipit voluptatem consequuntur unde.'
+        ]
+    ]]);
 });
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'RudyRacing',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorum similique, architecto distinctio velit facilis itaque dolorem non. Architecto quo dicta quaerat dolorum error, nulla exercitationem suscipit voluptatem consequuntur unde.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'JunaidiRacing',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorum similique, architecto distinctio velit facilis itaque dolorem non. Architecto quo dicta quaerat dolorum error, nulla exercitationem suscipit voluptatem consequuntur unde.'
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+    
+    return view('post' , ['title' => 'Single Post', 'post' => $post]);
+});
+
 
 Route::get('/contact', function () {
-    return view('contact', ['title' => 'ContactPage' ]);
+    return view('contact', ['title' => 'ContactPage']);
 });
