ca1b3d7c9dbced990614140bd426cf6a70e039e3
AdamJoestar <adamschoolauto@gmail.com>
2025-01-31 16:35:36 +0700

    n+1 problemo

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,6 +10,8 @@ class Post extends Model
 {
     use HasFactory;
     //protected $table = 'post'; buat apa ya?
+
+    protected $with = ['author', 'category'];
     protected $fillable = ['title', 'author', 'slug','body'];
 
     public function author():BelongsTo{
