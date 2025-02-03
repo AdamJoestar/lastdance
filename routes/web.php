ff7259580db67223c572aa9699fce6ddb19006ea
AdamJoestar <adamschoolauto@gmail.com>
2025-02-03 15:05:46 +0700

    Last commit pagination

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     // $posts = Post::with(['author','category'])->latest()->get();
 
-    return view('posts', ['title' => 'BlogPage', 'posts' => Post::filter(request(['search','category','author']))->latest()->get()]);
+    return view('posts', ['title' => 'BlogPage', 'posts' => Post::filter(request(['search','category','author']))->latest()->paginate(10)->withQueryString()]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
